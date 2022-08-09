1991a0f3de81b940aeaccb723e7a7317e0a42a84
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-08-09 18:16:58 +0200

    first

---
+++ README.md
@@ -0,0 +1,37 @@
+<div align="center">
+  <h1>cryptonets</h1>
+   
+</div>
+
+<div align="center">  
+Crypto Nets!
+</div>
+
+---
+
+<div align="center">
+<h4>Getting Started</h4>
+</div>
+  
+---
+  
+
+[![npm](https://img.shields.io/npm/v/cryptonets)](https://npmjs.com/package/cryptonets)
+[![npm](https://img.shields.io/npm/dw/cryptonets.svg)](https://npmjs.com/package/cryptonets)
+[![Github Stars](https://img.shields.io/github/stars/melvincarvalho/cryptonets.svg)](https://github.com/melvincarvalho/cryptonets/)
+
+## ⚡️ Features
+
+&nbsp;&nbsp;✓&nbsp; List Crypto Nets  
+&nbsp;&nbsp;✓&nbsp; Non proprietary  
+
+## ✍️ Getting Started
+
+---
+
+### Install with git
+
+```
+git clone https://github.com/melvincarvalho/cryptonets.git
+```
+
---
+++ bin/cryptonets.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+
+
+const BITCOIN = {
+  baseNetwork: 'bitcoin',
+  messagePrefix: '\x18Bitcoin Signed Message:\n',
+  bech32: 'bc',
+  bip32: {
+    public: 0x04b24746,
+    private: 0x04b2430c
+  },
+  pubKeyHash: 0x00,
+  scriptHash: 0x05,
+  wif: 0x80
+}
+
+const BITMARK = {
+  messagePrefix: '\x19BITMARK Signed Message:\n',
+  bech32: 'btmk',
+  bip32: {
+    public: 0x019da462,
+    private: 0x019d9cfe
+  },
+  pubKeyHash: 85,
+  scriptHash: 0x32,
+  wif: 213
+}
+
+const LIQUID = {
+  messagePrefix: '\x18Liquid Signed Message:\n',
+  bech32: 'ex',
+  blech32: 'lq',
+  bip32: {
+    public: 0x0488b21e,
+    private: 0x0488ade4
+  },
+  pubKeyHash: 57,
+  scriptHash: 39,
+  wif: 0x80,
+  confidentialPrefix: 12,
+  assetHash: '6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d'
+}
+
+const TESTNET3 = {
+  baseNetwork: 'testnet',
+  messagePrefix: '\x18Bitcoin Signed Message:\n',
+  bech32: 'tb',
+  bip32: {
+    public: 0x045f1cf6,
+    private: 0x045f18bc
+  },
+  pubKeyHash: 0x6f,
+  scriptHash: 0xc4,
+  wif: 0xef
+}
+
+const REGTEST = {
+  baseNetwork: 'regtest',
+  messagePrefix: '\x18Bitcoin Signed Message:\n',
+  bech32: 'bcrt',
+  bip32: {
+    public: 0x045f1cf6,
+    private: 0x045f18bc
+  },
+  pubKeyHash: 0x6f,
+  scriptHash: 0xc4,
+  wif: 0xef
+}
+
+const LITECOIN = {
+  coin: 'ltc',
+  bech32: 'ltc',
+  bip32: {
+    private: 27106558, // xprv_magic
+    public: 28471030 // xpub_magic_segwit_p2sh
+  },
+  dustThreshold: 0, // doesn't matter, for type correctness,
+  messagePrefix: 'Litecoin Signed Message:\n',
+  pubKeyHash: 48, // address_type
+  scriptHash: 50, // address_type_p2sh
+  wif: 128 // doesn't matter, for type correctness
+}
+
+const networks = [BITCOIN, LITECOIN, REGTEST, TESTNET3, LIQUID, BITMARK]
+
+console.log(networks)
+
